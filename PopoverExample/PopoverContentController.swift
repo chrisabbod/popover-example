b60f331e6db1f923a529399597b0e75d2ac53bf6
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-03-11 23:38:27 -0400

    Create PopoverContentController.swift and launch it when button is pressed

---
+++ PopoverExample/PopoverContentController.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverContentController.swift
+//  PopoverExample
+//
+//  Created by Chris Abbod on 3/11/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+
+class PopoverContentController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- PopoverExample/ViewController.swift
+++ PopoverExample/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showPopoverButtonAction(_ sender: Any) {
+        //get the button frame
+        /* 1 */
+        let button = sender as? UIButton
+        let buttonFrame = button?.frame ?? CGRect.zero
+        
+        /* 2 */
+        //Configure the presentation controller
+        let popoverContentController = self.storyboard?.instantiateViewController(withIdentifier: "PopoverContentController") as? PopoverContentController
+        popoverContentController?.modalPresentationStyle = .popover
+        
+        /* 3 */
+        if let popoverPresentationController = popoverContentController?.popoverPresentationController {
+            popoverPresentationController.permittedArrowDirections = .up
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.sourceRect = buttonFrame
+            popoverPresentationController.delegate = self
+            if let popoverController = popoverContentController {
+                present(popoverController, animated: true, completion: nil)
+            }
+        }
+    }
+    
 }
 
